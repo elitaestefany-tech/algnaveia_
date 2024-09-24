12412925b9f9089885deae0a529224de92c35fff
Élita Estefany  Bárbara <estefanytp2011@hotmail.com>
2024-09-23 21:29:21 -0300

    Add files via upload

---
+++ algnaveia1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream> // A biblioteca <iostream> está mantida
+
+int main() {
+    // Declaração das variáveis
+    int a, b, c;
+    int soma;
+
+    // Atribuição de valores às variáveis (exemplo)
+    a = 10;
+    b = 10;
+    c = 15;
+
+    // Cálculo da soma das variáveis
+    soma = a + b + c;
+
+    // Exibir o resultado da soma
+    printf("Soma = %d\n", soma);
+
+    return 0;
+}
